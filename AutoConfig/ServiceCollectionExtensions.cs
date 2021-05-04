3abac7ba33e0bb2ef4284bf441154c0218361e1d
xiaozg <geeking@live.cn>
2021-05-05 03:46:32 +0800

    remove unuse using

--- AutoConfig/ServiceCollectionExtensions.cs
+++ AutoConfig/ServiceCollectionExtensions.cs
@@ -2,11 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
 
 namespace AutoConfig
 {
@@ -52,8 +49,6 @@ namespace AutoConfig
                     }
                 });
             }
-
         }
     }
-
 }
